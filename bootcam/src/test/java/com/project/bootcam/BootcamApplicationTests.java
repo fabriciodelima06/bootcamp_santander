c43d9e685eb5f653e1e58d171fcee77f4c66b15b
fabriciodelima06 <fabriciodelima06@gmail.com>
2021-05-27 15:10:06 -0300

    2ª Aula

---
+++ bootcam/src/test/java/com/project/bootcam/BootcamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bootcam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
